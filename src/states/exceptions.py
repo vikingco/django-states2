e91d6ae1a583f2ea4e96a87c647336550689a39d
Jonathan Slenders <jonathan.slenders@city-live.be>
2011-03-01 10:41:07 +0100

    exceptions

---
+++ src/states/exceptions.py
@@ -0,0 +1,26 @@
+
+class TransitionException(Exception):
+    pass
+
+
+class TransitionOnUnsavedObject(TransitionException):
+    def __init__(self, instance):
+        Exception.__init__(self, "Cannot run state transition on unsaved object '%s'. "
+                "Please call save() on this object first." % instance)
+
+class PermissionDenied(TransitionException):
+    def __init__(self, instance, transition, user):
+        Exception.__init__(self, "Permission for executing the state '%s' has be denied to %s."
+                % (transition, user.get_full_name()))
+
+
+class UnknownTransition(TransitionException):
+    def __init__(self, instance, transition):
+        Exception.__init__(self, "Unknown transition '%s' on %s" %
+                    (transition, instance.__class__.__name__))
+
+
+class TransitionCannotStart(TransitionException):
+    def __init__(self, instance, transition):
+        Exception.__init__(self, "Transition '%s' on %s cannot start in the state '%s'" %
+                    (transition, instance.__class__.__name__, instance.value))
