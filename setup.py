fda3f07a0de814ae8c5e39024b058c7959af2cb8
Gert Van Gool <gertvangool@gmail.com>
2011-04-13 13:30:30 +0200

    Made setup.py fully pep8-compliant

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
+
 setup(
-    name = "django-states2",
-    version = "0.1",
-    url = 'https://github.com/citylive/django-states2',
-    license = 'BSD',
-    description = "State machine for django models",
-    long_description = open('README.rst','r').read(),
-    author = 'Jonathan Slenders, City Live nv',
-    packages = find_packages('src'),
-    package_dir = {'': 'src'},
-    classifiers = [
+    name="django-states2",
+    version="0.1",
+    url='https://github.com/citylive/django-states2',
+    license='BSD',
+    description="State machine for django models",
+    long_description=open('README.rst', 'r').read(),
+    author='Jonathan Slenders, City Live nv',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Operating System :: OS Independent',
@@ -18,4 +19,3 @@ setup(
         'Framework :: Django',
     ],
 )
-
