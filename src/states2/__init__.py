32705726b1e222139e13bffabb8b9f2784ff5aa8
Gert Van Gool <gertvangool@gmail.com>
2011-04-13 13:28:01 +0200

    Added version information to the module

---
+++ src/states2/__init__.py
@@ -0,0 +1,11 @@
+VERSION = (1, 2, 14)
+
+
+def get_version():
+    if len(VERSION) > 3 and VERSION[3] != 'final':
+        return '%s.%s.%s %s' % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
+    else:
+        return '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
+
+
+__version__ = get_version()
