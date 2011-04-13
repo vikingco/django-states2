6a59eea6a1c513bdfdacaca056b7be22779da44f
Gert Van Gool <gertvangool@gmail.com>
2011-04-13 12:19:30 +0200

    Cleaned-up code a little (show 404, ...)

--- states2/views.py
+++ states2/views.py
@@ -1,5 +1,7 @@
 from django.db.models import get_model
-from django.http import HttpResponseRedirect, HttpResponseForbidden, HttpResponse
+from django.http import (HttpResponseRedirect, HttpResponseForbidden,
+                         HttpResponse,)
+from django.shortcuts import get_object_or_404
 
 from states2.exceptions import PermissionDenied
 
@@ -8,7 +10,7 @@ def make_state_transition(request):
     if request.method == 'POST':
         app_label, model_name = request.POST['model_name'].split('.')
         model = get_model(app_label, model_name)
-        instance = model.objects.get(id=request.POST['id'])
+        instance = get_object_or_404(model, id=request.POST['id'])
         action = request.POST['action']
 
         if not hasattr(instance, 'make_transition'):
@@ -17,15 +19,13 @@ def make_state_transition(request):
         try:
             # Make state transition
             instance.make_transition(action, request.user)
-
+        except PermissionDenied, e:
+            return HttpResponseForbidden()
+        else:
             # ... Redirect to 'next'
             if 'next' in request.REQUEST:
                 return HttpResponseRedirect(request.REQUEST['next'])
             else:
                 return HttpResponse('OK')
-
-        except PermissionDenied, e:
-            return HttpResponseForbidden()
-
     else:
         return HttpResponseForbidden()
