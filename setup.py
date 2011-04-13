78a128b0561e43ed3ab6913b239c0411afc0ea89
Gert Van Gool <gertvangool@gmail.com>
2011-04-13 13:45:02 +0200

    Updated setup.py
    - Updated code dir
    - Automatically update version (based on __init__.py)

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
+import states2
 
 
 setup(
     name="django-states2",
-    version="0.1",
+    version=states2.__version__,
     url='https://github.com/citylive/django-states2',
     license='BSD',
     description="State machine for django models",
     long_description=open('README.rst', 'r').read(),
     author='Jonathan Slenders, City Live nv',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=find_packages('.'),
+    #package_dir={'': 'templates/*'},
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python',
