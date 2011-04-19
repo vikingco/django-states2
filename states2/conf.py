db2e58031f2900eff73f141920d86e41bc8d2cd6
Gert Van Gool <gertvangool@gmail.com>
2011-04-19 11:52:16 +0200

    Updated default log model name

--- states2/conf.py
+++ states2/conf.py
@@ -4,7 +4,5 @@ base_conf = getattr(settings, 'STATES2_CONF', {})
 
 # The model name for the state transition logs.
 # It will be string replaced with %(model_name)s and %(field_name)s
-# Example for when you use multiple ``StateField``s per object
-#     '%(model_name)s%(field_name)sLog'
-LOG_MODEL_NAME = base_conf.get(LOG_MODEL_NAME,
-                               '%(model_name)s_StateTransition')
+LOG_MODEL_NAME = base_conf.get('LOG_MODEL_NAME',
+                               '%(model_name)s%(field_name)sLog')
