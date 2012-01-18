3f1d56ba7572f13aa3a913a8e6584d5e9b2da708
Gert Van Gool <gertvangool@gmail.com>
2012-01-18 12:28:44 +0100

    Set the more documentation

--- states2/conf.py
+++ states2/conf.py
@@ -1,8 +1,10 @@
 from django.conf import settings
 
+
+#: The basic configuration
 base_conf = getattr(settings, 'STATES2_CONF', {})
 
 #: The model name for the state transition logs.
-#: It will be string replaced with %(model_name)s and %(field_name)s
+#: It will be string replaced with ``%(model_name)s`` and ``%(field_name)s``.
 LOG_MODEL_NAME = base_conf.get('LOG_MODEL_NAME',
                                '%(model_name)s%(field_name)sLog')
