e029499f01da04ab2651cdc0197c2977126b8848
Gert Van Gool <gertvangool@gmail.com>
2012-01-18 14:29:58 +0100

    Prettier indentation

--- states2/signals.py
+++ states2/signals.py
@@ -2,7 +2,7 @@ import django.dispatch
 
 #: Signal that is sent before a state transition is executed
 before_state_execute = django.dispatch.Signal(providing_args=['from_state',
-                                                                'to_state'])
+                                                              'to_state'])
 #: Signal that s sent after a state transition is executed
 after_state_execute = django.dispatch.Signal(providing_args=['from_state',
-                                                       'to_state'])
+                                                             'to_state'])
