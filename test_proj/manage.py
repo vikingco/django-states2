7b15cc4cc0fdb8bad6515591b36278df32bc3b7c
Steven Klass <sklass@7stalks.com>
2013-01-28 15:14:35 -0500

    Fixed the settings calls..

--- test_proj/manage.py
+++ test_proj/manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from django.conf import settings
 from django.core.management import execute_manager
 import imp
 try:
@@ -8,7 +9,6 @@ except ImportError:
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
     sys.exit(1)
 
-import settings
 
 if __name__ == "__main__":
     execute_manager(settings)
