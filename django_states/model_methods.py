fc9f7a6a7bd8b273a0e75ddcc2835ede680c144f
Linsy Aerts <linsy.aerts@mobilevikings.com>
2013-11-07 16:22:52 +0100

    fix when user is None

--- django_states/model_methods.py
+++ django_states/model_methods.py
@@ -170,7 +170,7 @@ def get_STATE_info(self, field='state', machine=None):
                 except TypeError:
                     serialized_kwargs = json.dumps(None)
 
-                if not user.is_authenticated():
+                if user and not user.is_authenticated():
                     user=None
 
                 transition_log = _state_log_model.objects.create(
