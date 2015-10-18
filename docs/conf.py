eb34594d1498ceb95c42532d3af2eaa56115b835
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-10-18 03:06:27 +0200

    Add requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'dataentry.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'test_proj.settings'
 from django.conf import settings
 
 # -- General configuration -----------------------------------------------------
