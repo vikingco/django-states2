9bfdc989553dd5b901e025de621583b33e877231
Steven Klass <rh0dium@users.noreply.github.com>
2018-10-02 16:58:24 -0700

    Updated to support version

--- django_states/__init__.py
+++ django_states/__init__.py
@@ -9,7 +9,7 @@ from __future__ import absolute_import
 
 __name__ = 'django_states'
 __author__ = 'Pivotal Energy Solutions'
-__version_info__ = (1, 1, 0)
+__version_info__ = (1, 6, 9)
 __version__ = '.'.join(map(str, __version_info__))
 __date__ = '2014/07/22 4:47:00 PM'
 __credits__ = ['Jonathan Slenders', 'Ben Mason', 'Dirk Moors', 'Gert Van Gool', 'Giovanni Collazo', 'Jakub Paczkowski',
