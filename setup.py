7dcf7fd6cb489ec89a5e44b125283762ff3d3295
Steven Klass <rh0dium@users.noreply.github.com>
2018-10-02 16:50:54 -0700

    Update version to support 1.11

--- setup.py
+++ setup.py
@@ -141,7 +141,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['django>=1.7,<1.9'],  # Optional
+    install_requires=['django>=1.11,<2.0'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
