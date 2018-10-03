92f006c585a2174cb6d321bcd247c39527ccb51d
Steven Klass <rh0dium@users.noreply.github.com>
2018-10-02 17:06:14 -0700

    Fix URLs I thought they would have been corrected by this point.

--- django_states/compat.py
+++ django_states/compat.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 # Django >= 1.4 moves handler404, handler500, include, patterns and url from
 # django.conf.urls.defaults to django.conf.urls.
 try:
-        from django.conf.urls import (patterns, url, include)
+        from django.conf.urls import url, include
 except ImportError:
-        from django.conf.urls.defaults import (patterns, url, include)
+        from django.conf.urls.defaults import url, include
--- django_states/urls.py
+++ django_states/urls.py
@@ -2,9 +2,9 @@
 """Urls"""
 from __future__ import absolute_import
 
-from .compat import patterns, url
+from .compat import url
 from django_states.views import make_state_transition
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^make-state-transition/$', make_state_transition, name='django_states_make_transition'),
-)
+]
